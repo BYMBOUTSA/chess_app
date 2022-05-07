9789eac7bac4e17bed14542ee2552e05dfd9afbe
BYMBOUTSA <charlymoundounga3@gmail.com>
2022-05-07 22:32:59 +0200

    Create name player with a button of success

--- streamlit_code/main.py
+++ streamlit_code/main.py
@@ -3,3 +3,8 @@
 import streamlit as st
 
 st.write("Hello world !")
+
+player_name = st.text_input("Quel est votre nom ?", "Saisissez votre nom ici ...")
+
+if st.button("value"):
+    st.success(player_name)
