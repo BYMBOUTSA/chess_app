be0800e605363452324534344430f11b614efb69
BYMBOUTSA <charlymoundounga3@gmail.com>
2022-05-08 23:27:26 +0200

    Creation of a dictionary to retrieve our first 50 values from our players' games.

--- streamlit_code/main.py
+++ streamlit_code/main.py
@@ -3,6 +3,9 @@
 import streamlit as st
 import pandas as pd
 
+# Our imports
+from lichess_api.games_data import get_games_from_player
+
 st.write("Hello world !")
 
 player_name = st.text_input("Quel est votre nom ?", "Saisissez votre nom ici ...")
@@ -10,4 +13,9 @@ player_name = st.text_input("Quel est votre nom ?", "Saisissez votre nom ici ...
 if st.button("value"):
     st.success(player_name)
 
-# Faire appel à l'API lichess pour recevoir des données sur les 50 dernières partie d'échecs du joueur.
+    # Faire appel à l'API lichess pour recevoir des données sur les 50 dernières partie d'échecs du joueur.
+    df = get_games_from_player(player_name, 50)
+
+    # Show the result
+    st.dataframe(df)
+
